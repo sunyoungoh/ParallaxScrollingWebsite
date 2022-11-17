6c3a2a500e088462b617ae8fe57c7abc7e11d143
sunyoungoh <tjsdud7379@gmail.com>
2022-11-17 18:41:25 +0900

    Add scroll sections active link

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,6 @@ let mountain_behind = document.getElementById('mountain_behind');
 let text = document.getElementById('text');
 let btn = document.getElementById('btn');
 let mountain_front = document.getElementById('mountain_front');
-let header = document.querySelector('header');
-let main = document.getElementById('main');
-let mainHeight = main.getBoundingClientRect().height;
 
 window.addEventListener('scroll', function () {
   let value = window.scrollY;
@@ -18,9 +15,38 @@ window.addEventListener('scroll', function () {
   text.style.marginTop = value * 1.5 + 'px';
   btn.style.marginTop = value * 1.5 + 'px';
   // header.style.top = value * 0.5 + 'px'
-  if (window.scrollY > mainHeight) {
-    header.classList.add('bg-color');
-  } else {
-    header.classList.remove('bg-color');
-  }
 });
+/*============= CHANGE BACKGROUND HEADER ============*/
+function scrollHeader() {
+  const header = document.querySelector('header');
+  const home = document.getElementById('home');
+  const homeHeight = home.getBoundingClientRect().height;
+
+  if (window.scrollY > homeHeight) header.classList.add('scroll-header');
+  else header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
+
+/*============ SCROLL SECTIONS ACTIVE LINK ============*/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 100;
+    const sectionId = current.getAttribute('id');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.add('active');
+    } else {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.remove('active');
+    }
+  });
+}
+window.addEventListener('scroll', scrollActive);
