bba64299a97f8d73de987b4f81bf48b2c5cd7dc8
sunyoungoh <tjsdud7379@gmail.com>
2022-11-19 16:01:13 +0900

    Edit media query

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,25 @@
+/*============ MENU SHOW Y HIDDEN ============*/
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
+
+/*============ MENU SHOW ============*/
+/* validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
+
+/*============ MENU HIDDEN ============*/
+/* validate if constant exists */
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
+
+/*============ SCROLL ============*/
 let stars = document.getElementById('stars');
 let moon = document.getElementById('moon');
 let mountain_behind = document.getElementById('mountain_behind');
@@ -16,14 +38,24 @@ window.addEventListener('scroll', function () {
   btn.style.marginTop = value * 1.5 + 'px';
   // header.style.top = value * 0.5 + 'px'
 });
+
 /*============= CHANGE BACKGROUND HEADER ============*/
 function scrollHeader() {
   const header = document.querySelector('header');
   const home = document.getElementById('home');
   const homeHeight = home.getBoundingClientRect().height;
+  const logo = document.getElementById('logo');
+  const navMenu = document.getElementById('nav-menu');
 
   if (window.scrollY > homeHeight) header.classList.add('scroll-header');
   else header.classList.remove('scroll-header');
+  if (window.scrollY > homeHeight - 50 && window.innerWidth < 568) {
+    logo.classList.add('hide-logo');
+    navMenu.classList.add('scroll-nav__menu');
+  } else {
+    logo.classList.remove('hide-logo');
+    navMenu.classList.remove('scroll-nav__menu');
+  }
 }
 window.addEventListener('scroll', scrollHeader);
 
